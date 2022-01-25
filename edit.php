97ddb203d0285c4576858340b1103c093bc8e88c
ignattru <ignatusha.evgeny@gmail.com>
2022-01-25 10:47:23 +0300

    Delete auth old

--- edit.php
+++ edit.php
@@ -1,4 +1,3 @@
-<?php include __DIR__.'/config.php'; ?>
 <html>
   <head>
     <title>Редактировать задачу</title>
@@ -11,7 +10,6 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" charset="utf-8">
   </head>
-  <?php if(AUTH) { //Если мы авторизированы ?>
 <body>
   <style>
   .page-header {
@@ -60,7 +58,7 @@ $row = mysqli_fetch_row($result);
 
 if(isset($_POST['dfact']) && isset($_POST['report']) && isset($_POST['statustask']) && isset($_POST['title']) && isset($_POST['body']) && isset($_POST['important']) && isset($_POST['maker'])){
     
-    // экранирования символов
+    // Screen
     $dfact = $_POST['dfact']; 
     $report = $_POST['report']; 
     $title = strip_tags($_POST['title']); 
@@ -69,16 +67,13 @@ if(isset($_POST['dfact']) && isset($_POST['report']) && isset($_POST['statustask
     $important = $_POST['important'];
     $maker = $_POST['maker'];
     
-    // создание строки запроса 
-    $sql_insert_report = "INSERT INTO REPORT VALUES(NULL, '$report')";
-    
+    // Construct query 
+    $sql_insert_report = "INSERT INTO REPORT VALUES(NULL, '$report')";   
     $query2 ="UPDATE TASK_LIST SET T_FACTDATE = '$dfact', T_REPORT = (SELECT MAX(OUID) FROM REPORT), T_STATUS_TASK = '$statustask', T_TITLE = '$title', T_BODY = '$body', T_IMPORTANT = '$important', T_MAKER = '$maker' WHERE TASK_LIST.OUID = $id ";
-    
      $resultinsert = mysqli_query($link, $sql_insert_report) or die("Ошибка " . mysqli_error($link)); 
     
     $result2 = mysqli_query($link, $query2) or die("Статус задачи не выбран!"); 
-    if($result2)
-    {
+    if($result2) {
         echo "<div class='alert alert-success' role='alert'>Задача успешно обновлена</div>";
     }
    mysqli_free_result($result2);
@@ -91,7 +86,6 @@ if(isset($_POST['dfact']) && isset($_POST['report']) && isset($_POST['statustask
     <b style="color:#fff;">&nbsp;&nbsp;Задача</b>
     <span class="label label-default"><?php echo $row[0]; ?></span>
   </div>
-
 <div class="container" style="background-color:#fbfbfb";>
 <div class="page-header">
         <h2>
@@ -102,24 +96,19 @@ if(isset($_POST['dfact']) && isset($_POST['report']) && isset($_POST['statustask
         </p>
 </div>
   <div class="row">
-
   <form method="POST">
-
 <div class="form-row">
     <div class="form-group col-md-12">
     <label for="inputAddress">Заголовок:</label>
     <input type="text" class="form-control" id="inputAddress" value="<?php echo $row[1]; ?>" name="title">
     </div>
   </div>
-
 <div class="form-row">
     <div class="form-group col-md-12">
     <label for="exampleFormControlTextarea3">Описание</label>
     <textarea class="form-control" name="body" id="editor" placeholder="Описание задачи не более 700 символов" rows="3"><?php echo $row[2]; ?></textarea>
     </div>
   </div>
-
-
   <div class="form-group">
    <div class="form-group col-md-6">
     <label for="inputState">Сменить исполнителя:</label>
@@ -128,7 +117,6 @@ if(isset($_POST['dfact']) && isset($_POST['report']) && isset($_POST['statustask
   $sqlMaker = "SELECT usr.OUID, usr.A_NAME FROM USERS usr";
   $linkMaker = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($linkMaker));
   $linkMaker->set_charset("utf8"); 
-
     $resultMaker = mysqli_query($linkMaker, $sqlMaker) or die("Ошибка " . mysqli_error($linkMaker)); 
     if($resultMaker) {   
         while ($rowMaker = mysqli_fetch_row($resultMaker)) { 
@@ -148,7 +136,7 @@ if(isset($_POST['dfact']) && isset($_POST['report']) && isset($_POST['statustask
   
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputState">Важно?</label>
+      <label for="inputState">Важность</label>
       <select id="inputState" class="form-control" name="important">
          <?php 
   $sqlIfImport = "SELECT imp.OUID, imp.A_NAME FROM IMPORTANT imp";
@@ -170,7 +158,6 @@ if(isset($_POST['dfact']) && isset($_POST['report']) && isset($_POST['statustask
 ?>
       </select>
     </div>
-  
   <div class="form-row">
     <div class="form-group">
       <div class="form-group col-md-6">
@@ -254,98 +241,11 @@ if(isset($_POST['dfact']) && isset($_POST['report']) && isset($_POST['statustask
         } );
   </script>
 <?php
- // закрываем подключение
+ // Closed connection
 mysqli_free_result($result);
 mysqli_close($link);
 ?>
 
-<?php } else { //Если мы не авторизированы  ?>
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" />
-<style>
-.form-width {max-width: 25rem;}
-.has-float-label {
- position: relative; }
- .has-float-label label {
- position: absolute;
- left: 0;
- top: 0;
- cursor: text;
- font-size: 75%;
- opacity: 1;
- -webkit-transition: all .2s;
- transition: all .2s;
- top: -.5em;
- left: 0.75rem;
- z-index: 3;
- line-height: 1;
- padding: 0 1px; }
- .has-float-label label::after {
- content: " ";
- display: block;
- position: absolute;
- background: white;
- height: 2px;
- top: 50%;
- left: -.2em;
- right: -.2em;
- z-index: -1; }
- .has-float-label .form-control::-webkit-input-placeholder {
- opacity: 1;
- -webkit-transition: all .2s;
- transition: all .2s; }
- .has-float-label .form-control:placeholder-shown:not(:focus)::-webkit-input-placeholder {
- opacity: 0; }
- .has-float-label .form-control:placeholder-shown:not(:focus) + label {
- font-size: 150%;
- opacity: .5;
- top: .3em; }
-
-.input-group .has-float-label {
- display: table-cell; }
- .input-group .has-float-label .form-control {
- border-radius: 0.25rem; }
- .input-group .has-float-label:not(:last-child) .form-control {
- border-bottom-right-radius: 0;
- border-top-right-radius: 0; }
- .input-group .has-float-label:not(:first-child) .form-control {
- border-bottom-left-radius: 0;
- border-top-left-radius: 0;
- margin-left: -1px; }
- body {
-    background: linear-gradient(to bottom, #ff9900 0%, #ffcc00 100%);
- }
-</style>
-
-<div class="p-x-1 p-y-3">
-    <form class="card card-block m-x-auto bg-faded form-width" action="login.php" method="post">
-    <legend class="m-b-1 text-xs-center">Войти</legend>
-    <div class="form-group input-group">
-    <span class="has-float-label">
-    <input class="form-control" id="first" type="text" name="login" placeholder="Логин"/>
-    <label for="first">Логин</label>
-    </span>
-    </div>
-    <div class="form-group has-float-label">
-    <input class="form-control" id="password" type="password" name="password" placeholder="••••••••"/>
-    <label for="password">Пароль</label>
-    </div>
-    <div class="form-group">
-     <label class="custom-control custom-checkbox">
-    <input class="custom-control-input" type="checkbox" name="remember"/>
-    <span class="custom-control-indicator"></span>
-    <span class="custom-control-description">Запомнить меня</span>
-    </label>
-    <?php if(!empty($message)) { ?>
-    <p><?php echo $message; ?></p>
-    <?php } ?>
-    </div>
-    <div class="text-xs-center">
-    <button class="btn btn-block btn-warning" type="submit">Вход</button>
-    </div>
-    </form>
-</div>
-<?php } ?>
 
 </body>
 </html>
