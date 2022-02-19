56f680dd58df569b8e4571085fd922bb77377658
ignattru <ignatusha.evgeny@gmail.com>
2022-02-19 23:23:14 +1000

    Close auth

--- task.php
+++ task.php
@@ -9,7 +9,6 @@
         <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     </head>
-    <?php if(AUTH) { //Если мы авторизированы ?>
 <body>
 
 <?php
@@ -22,31 +21,18 @@
 	
 $query ="
   SELECT
- /*row[0] OUID таска */
   tList.OUID AS idTask  
- /*row[1] заголовок*/
   ,tList.T_TITLE AS title 
- /* row[2] описание*/
   ,IFNULL(tList.T_BODY, 'Нет описания')
- /* row[3] дата создания*/
   ,tList.T_CREATEDATE AS dCreate
- /* row[4] дата дедлайна*/
   ,tList.T_PLANDATE AS dPlan 
- /* row[5] дата фактического исполнения*/
   ,tList.T_FACTDATE AS dFact 
- /* row[6] текст отчета*/
   ,report.A_NAME AS report   
- /* row[7] важно?*/
   ,imp.A_NAME AS isImportant 
- /* row[8] имя исполнителя*/
   ,users.A_NAME AS users      
- /* row[9] статус задачи*/
   ,tStatus.A_NAME AS statusTask  
-  /* row[10] OUID (важно)*/
   ,imp.OUID AS isimpid
- /* row[11] OUID статуса задачи */
   ,tStatus.OUID AS statId 
- /* row[12] текущая дата*/
   ,(SELECT CURDATE()) AS timestmp
 
   FROM TASK_LIST tList
@@ -119,92 +105,6 @@ $row = mysqli_fetch_row($result);
   mysqli_free_result($result);
   mysqli_close($link);
 ?>
-<?php } else { //Если мы не авторизированы  ?>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" />
-<style>
-.form-width {max-width: 25rem;}
-.has-float-label {
- position: relative; }
- .has-float-label label {
- position: absolute;
- left: 0;
- top: 0;
- cursor: text;
- font-size: 75%;
- opacity: 1;
- -webkit-transition: all .2s;
- transition: all .2s;
- top: -.5em;
- left: 0.75rem;
- z-index: 3;
- line-height: 1;
- padding: 0 1px; }
- .has-float-label label::after {
- content: " ";
- display: block;
- position: absolute;
- background: white;
- height: 2px;
- top: 50%;
- left: -.2em;
- right: -.2em;
- z-index: -1; }
- .has-float-label .form-control::-webkit-input-placeholder {
- opacity: 1;
- -webkit-transition: all .2s;
- transition: all .2s; }
- .has-float-label .form-control:placeholder-shown:not(:focus)::-webkit-input-placeholder {
- opacity: 0; }
- .has-float-label .form-control:placeholder-shown:not(:focus) + label {
- font-size: 150%;
- opacity: .5;
- top: .3em; }
-
-.input-group .has-float-label {
- display: table-cell; }
- .input-group .has-float-label .form-control {
- border-radius: 0.25rem; }
- .input-group .has-float-label:not(:last-child) .form-control {
- border-bottom-right-radius: 0;
- border-top-right-radius: 0; }
- .input-group .has-float-label:not(:first-child) .form-control {
- border-bottom-left-radius: 0;
- border-top-left-radius: 0;
- margin-left: -1px; }
- body {
-    background: linear-gradient(to bottom, #ff9900 0%, #ffcc00 100%);
- }
-</style>
-
-<div class="p-x-1 p-y-3">
-    <form class="card card-block m-x-auto bg-faded form-width" action="login.php" method="post">
-    <legend class="m-b-1 text-xs-center">Войти</legend>
-    <div class="form-group input-group">
-    <span class="has-float-label">
-    <input class="form-control" id="first" type="text" name="login" placeholder="Логин"/>
-    <label for="first">Логин</label>
-    </span>
-    </div>
-    <div class="form-group has-float-label">
-    <input class="form-control" id="password" type="password" name="password" placeholder="••••••••"/>
-    <label for="password">Пароль</label>
-    </div>
-    <div class="form-group">
-     <label class="custom-control custom-checkbox">
-    <input class="custom-control-input" type="checkbox" name="remember"/>
-    <span class="custom-control-indicator"></span>
-    <span class="custom-control-description">Запомнить меня</span>
-    </label>
-    <?php if(!empty($message)) { ?>
-    <p><?php echo $message; ?></p>
-    <?php } ?>
-    </div>
-    <div class="text-xs-center">
-    <button class="btn btn-block btn-warning" type="submit">Вход</button>
-    </div>
-    </form>
-</div>
-<?php } ?>
 </body>
 </html>
